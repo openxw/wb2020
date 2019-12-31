ed8a3905a3e3b196241072c99e01f5f4a08a514f
Alan <openxw@qq.com>
2019-12-31 17:27:57 +0800

    用户注册页面

--- public/js/app.js
+++ public/js/app.js
@@ -37071,8 +37071,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/Code/laravel6/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/Code/laravel6/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/Code/Laravel6/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/Code/Laravel6/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
