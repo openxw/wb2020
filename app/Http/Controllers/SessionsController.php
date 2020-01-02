8c402aa85f5acc4e756b54ff1274335647600ed3
Alan <openxw@qq.com>
2020-01-02 15:26:58 +0800

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -21,7 +21,7 @@ class SessionsController extends Controller
             'password'=>'required'
         ]);
 
-        if(Auth::attempt($credentials)){
+        if(Auth::attempt($credentials,$request->has('remmeber'))){
             session()->flash('success'.'欢迎回来！');
             return redirect()->route('users.show',[auth::user()]);
         }else{
