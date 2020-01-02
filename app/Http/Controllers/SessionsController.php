9f7286643b424170e9fed87a3a83263097af399c
Alan <openxw@qq.com>
2020-01-02 19:32:44 +0800

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,14 @@ use Auth;
 class SessionsController extends Controller
 {
     //
+    public function __construct()
+    {
+        # code...
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         # code...
@@ -23,7 +31,8 @@ class SessionsController extends Controller
 
         if(Auth::attempt($credentials,$request->has('remmeber'))){
             session()->flash('success'.'欢迎回来！');
-            return redirect()->route('users.show',[auth::user()]);
+            $fallback = route('users.show',Auth::user());
+            return redirect()->intended($fallback);
         }else{
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,18 @@ use Auth;
 class UsersController extends Controller
 {
     //
+    public function __construct()
+    {
+        # code...
+        $this->middleware('auth',[
+            'except' => ['show','create','store']
+        ]);
+
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         # code...
@@ -52,12 +64,14 @@ class UsersController extends Controller
     public function edit(User $user)
     {
         # code...
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     public function update(User $user,Request $request)
         {
             # code...
+            $this->authorize('update',$user);
             $this->validate($request,[
                 'name'=>'required|max:50',
                 'password'=>'nullable|confirmed|min:6',
