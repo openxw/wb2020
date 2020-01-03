f0511396677377e1d5bab39579154faee3ea5ad5
Alan <openxw@qq.com>
2020-01-03 15:42:42 +0800

    社交统计信息

---
+++ database/seeds/FollowersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class FollowersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $users = User::all();
+        $user = $users->first();
+        $user_id = $user->id;
+
+        //获取去除掉 ID 为 1 的所有用户 ID 数组
+        $followers = $users->slice(1);
+        //除了1号用户以为的所有用户都来关注 1 号用户
+        foreach ($followers as $follower) {
+            # code...
+            $follower->follow($user_id);
+        }
+
+    }
+}
