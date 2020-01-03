ed37fcd74c453344381152d9c1242e3a72897fb6
Alan <openxw@qq.com>
2020-01-03 12:56:47 +0800

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -28,4 +28,14 @@ class StatusesController extends Controller
         session()->flash('success','发布成功!');
         return redirect()->back();
     }
+
+    public function destroy(Status $status)
+    {
+        # code...
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success','微博已被成功删除!');
+        return redirect()->back();
+    }
+
 }
